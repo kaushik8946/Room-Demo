af1d98bfe6fac6f0d61e1e012d321a0ac497b568
Surya Kaushik <suryakaushik2003@gmail.com>
2024-01-25 22:33:26 +0530

    first commit

---
+++ app/src/main/java/com/kaushik/roomdemo/screens/InsertScreen.kt
@@ -0,0 +1,30 @@
+package com.kaushik.roomdemo.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun InsertScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var firstName by rememberSaveable {
+            mutableStateOf("")
+        }
+        var lastName by rememberSaveable {
+            mutableStateOf("")
+        }
+
+
+    }
+}
